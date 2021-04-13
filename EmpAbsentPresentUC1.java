ab7276d66e4ee6ec363f2cc6dc4d6ad61d1aa0de
MeghaManwal <meghamanwal3926@gmail.com>
2021-04-13 12:11:38 +0530

    Added: To Check if Employee is Present or Absent

---
+++ EmpAbsentPresentUC1.java
@@ -0,0 +1,11 @@
+
+public class EmpAbsentPresentUC1 {
+	public static void main(String[]args) {
+ 	 int is_Present=1;
+	 double empCheck=Math.floor(Math.random()*10) % 2;
+	if (empCheck == is_Present)
+  	 System.out.println("Employee is Present");
+	else
+ 	 System.out.println("Employee is absent");
+     }
+ }
